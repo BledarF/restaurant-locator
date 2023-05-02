76796b3471b2002072617db231859158bb0dcb75
BledarF <BledarF@hotmail.com>
2023-05-02 15:05:08 +0100

    Complete login page

--- restaurant-locator/src/pages/login/LoginPage.tsx
+++ restaurant-locator/src/pages/login/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, Grid } from "@mui/material";
 import { LoginForm } from "./LoginForm";
+import ManEating from "../../assets/images/ManEating.png";
 import "../../styles/pages/login/_login.scss";
 
 export const LoginPage = () => {
@@ -8,7 +9,7 @@ export const LoginPage = () => {
     <Card className="login-page-container">
       <LoginForm />
       <Grid className="image-container">
-        <img alt="food img" />
+        <img src={ManEating} alt="food img" className="img" />
       </Grid>
     </Card>
   );
